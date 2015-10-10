4804f266829229b637ba6769bededa26eaa2579b
THALES SANTOS <thales.angelino@gmail.com>
2015-10-10 02:54:56 -0300

    Mocha tests added to the RevAPM project

---
+++ README.md
@@ -0,0 +1,1 @@
+# This is an test suite for the API of the project RevAPM.
---
+++ test/tests.js
@@ -0,0 +1,103 @@
+var should = require('should');
+var assert = require('assert');
+var request = require('supertest');
+
+describe('Activity Suite', function () {
+    var config = {
+        user: 'USERNAME',
+        pwd: 'PASSWORD',
+        wrongPwd: 'WRONG_PASSWORD',
+        url: 'https://api.revsw.net/'
+    };
+
+    before(function (done) {
+        console.info('Test Suite Started');
+        done();
+    });
+
+    describe('Activity Endpoint Tests', function () {
+
+        it('should return success', function (done) {
+            request(config.url).get('/v1/activity')
+                .auth(config.user, config.pwd)
+                .expect(200)
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    done();
+                });
+        });
+
+        it('should fail when user is unauthorized', function (done) {
+            request(config.url).get('/v1/activity')
+                .auth(config.user, config.wrongPwd)
+                .expect(401)
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    done();
+                });
+        });
+
+        it('should fail when wrong parameters are used', function (done) {
+            request(config.url).get('/v1/activity')
+                .query({ from_timestamp:1, to_timestamp:0 })
+                .auth(config.user, config.pwd)
+                .expect(400)
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    done();
+                });
+        });
+    });
+
+    describe('Activity/Summary Endpoint Tests', function () {
+
+        it('should return success', function (done) {
+            request(config.url).get('/v1/activity/summary')
+                .auth(config.user, config.pwd)
+                .expect(200)
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    done();
+                });
+        });
+
+        it('should fail when user is unauthorized', function (done) {
+            request(config.url).get('/v1/activity/summary')
+                .auth(config.user, config.wrongPwd)
+                .expect(401)
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    done();
+                });
+        });
+
+        it('should fail when wrong parameters are used', function (done) {
+            request(config.url).get('/v1/activity/summary')
+                .query({ from_timestamp:1, to_timestamp:0 })
+                .auth(config.user, config.pwd)
+                .expect(400)
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    done();
+                });
+        });
+    });
+
+    after(function (done) {
+        console.info('Test Suite Finished');
+        done();
+    });
+
+});
